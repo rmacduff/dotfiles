d487658f90e6a1a9f09580b2ef28f0ecd98938dd
Ross Macduff <ross@macduff.ca>
2020-12-12 12:07:47 -0500

    add function for creating private github repo

--- bash/.bashrc.d/01_funcs.sh
+++ bash/.bashrc.d/01_funcs.sh
@@ -5,3 +5,18 @@
 # Custom functions for specific environments
 # shellcheck source=/dev/null
 . "$HOME"/.bash.d/functions/*.sh
+
+mk_gh_repo() {
+  if [[ $# -ne 1 ]]; then
+    echo "Need to specify the name of the repo to create."
+    return
+  fi
+  curl -i -H "Authorization: token ${GITHUB_TOKEN}" \
+    -d "{ \
+        \"name\": \"$1\", \
+        \"auto_init\": true, \
+        \"private\": true, \
+        \"gitignore_template\": \"nanoc\" \
+      }" \
+    https://api.github.com/user/repos
+}
